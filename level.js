f19d01282af18c3115dd909f696d8f13aa8d538c
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-14 15:03:38 +0600

    js test if else

---
+++ level.js
@@ -0,0 +1,11 @@
+/**
+ * multi level condition
+ */
+
+const price = 5000;
+
+if (price >= 5000) {
+  const discount = (price * 10) / 100;
+  const payAmount = price - discount;
+  console.log(payAmount);
+}
---
+++ multi.js
@@ -0,0 +1,18 @@
+// const salary= 2500;
+// const isBcs = true;
+
+// if(salary>20000){
+//     console.log('onek valo chele')
+// }
+// else{
+//     console.log('cheler salary kom biye dish na')
+// }
+const salary = 35000;
+const height = 65;
+const isBcs = true;
+
+if (salary < 25000 || height < 72) {
+  console.log("job done");
+} else {
+  console.log("na mama hoilo na");
+}

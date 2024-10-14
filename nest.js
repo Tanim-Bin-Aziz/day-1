7d8697f48a9e3cfef15893414c69a6a498e86899
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-14 15:47:30 +0600

    nest js test

---
+++ nest.js
@@ -0,0 +1,14 @@
+const money = 50;
+if (money > 300) {
+  console.log("bro youre rich");
+} else {
+  if (money > 100) {
+    console.log("tui gorib o na borolok o na");
+  } else {
+    if (money > 0) {
+      console.log("fokir");
+    } else {
+      console.log("borolok");
+    }
+  }
+}
---
+++ resturant.js
@@ -0,0 +1,10 @@
+const age = 17;
+const price = 500;
+
+if (age <= 12) {
+  console.log("you can get free food");
+} else if (age >= 60) {
+  console.log("get 50% discount");
+} else {
+  console.log("pay regular price");
+}

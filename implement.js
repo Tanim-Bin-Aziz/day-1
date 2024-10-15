a3f75403f3e184471356686cee04bd92da12631b
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-15 21:24:05 +0600

    implement test done

---
+++ implement.js
@@ -0,0 +1,12 @@
+// let n = 5;
+// //n++;
+// console.log(++n); // pre implement
+
+// console.log(n++); // post implement
+// console.log(n);
+
+let n = 5;
+let m = 10 + ++n;
+console.log(m + " " + n++ + " " + ++n);
+// console.log(m);
+// console.log(n);

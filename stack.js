86529ccb6e33f4cd1081339fd6905506dbcd5218
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-15 20:02:32 +0600

    stack js test

---
+++ stack.js
@@ -0,0 +1,8 @@
+console.log("Arithematic Oparators");
+let a = 20,
+  b = 10;
+console.log(a + "+" + b + "=" + (a + b)); //20 + 10 = 30
+console.log(a + "+" + b + "=" + (a - b)); //20 - 10 = 10
+console.log(a + "+" + b + "=" + a * b); //20 * 10 = 200
+console.log(a + "+" + b + "=" + a / b); //20 / 10 = 2
+console.log(a + "+" + b + "=" + (a % b)); //20 % 10 = 0

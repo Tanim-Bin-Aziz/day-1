8f44d7b82e9181fb2b4137e9269709cd19897037
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-15 20:36:51 +0600

    relation js test done

---
+++ relation.js
@@ -0,0 +1,11 @@
+let m = 10,
+  n = 15,
+  o = 10;
+console.log(m + ">" + n + "=" + (m > n)); //m > n == false
+console.log(m + "<" + n + "=" + (m < n)); // m < n == true
+console.log(m + ">=" + o + "=" + (m >= o)); // m >= n == true
+console.log(m + "=<" + n + "=" + (m <= n)); // m =< n == true
+console.log(m + "==" + n + "=" + (m == n)); // m == n false
+console.log(m + "==" + o + "=" + (m == o)); // m == o true
+console.log(m + "=!" + n + "=" + (m != n)); // m != n true
+console.log(m + "!=" + o + "=" + (m != o)); //false
--- stack.js
+++ stack.js
@@ -6,3 +6,19 @@ console.log(a + "+" + b + "=" + (a - b)); //20 - 10 = 10
 console.log(a + "+" + b + "=" + a * b); //20 * 10 = 200
 console.log(a + "+" + b + "=" + a / b); //20 / 10 = 2
 console.log(a + "+" + b + "=" + (a % b)); //20 % 10 = 0
+
+// Assignment Oparator
+
+console.log("Assignment Oprators");
+let aso = 10; //10
+console.log("a =" + aso); // 10
+aso += 5; // 10 + 5
+console.log("a +=" + aso); // 15
+aso -= 5; // 15 - 5
+console.log("a -=" + aso); // 10
+aso *= 5; // 10 * 5
+console.log("a *=" + aso); // 50
+aso /= 5; // 50 / 5
+console.log("a /=" + aso); // 10
+aso %= 4; // 10 % 4
+console.log("a %=" + aso);

79afc59e892fedad327fb12528240b0a5348ff05
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-17 13:53:33 +0600

    theme test

---
+++ theme.js
@@ -0,0 +1,11 @@
+const theme = "dark";
+
+// let bgColor = "";
+
+let bgColor = theme == "dark" ? "#333333" : "#efefef";
+
+// if (theme == "dark") {
+//   bgColor = "#336748";
+// } else {
+//   bgColor = "#efefef";
+// }

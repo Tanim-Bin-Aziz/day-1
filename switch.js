62529fd21da06b1b86186bb184af181ec16c1143
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-17 13:40:44 +0600

    switch case test 1

--- switch.js
+++ switch.js
@@ -1,6 +1,25 @@
-const day = 0;
+const day = 4;
 
 switch (day) {
   case 0:
     console.log("saturday");
+    break;
+  case 1:
+    console.log("Sunday");
+    break;
+  case 2:
+    console.log("Monday");
+    break;
+  case 3:
+    console.log("Tuesday");
+    break;
+  case 4:
+    console.log("Wednesday");
+    break;
+  case 5:
+    console.log("Thursday");
+    break;
+  case 6:
+    console.log("Friday");
+    break;
 }

5dbb68a616ad388075693201911ca44c19ac72c1
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-17 14:22:21 +0600

    forLoop test done

--- forLoop.js
+++ forLoop.js
@@ -1,3 +1,9 @@
-for (let i = 1; i <= 100; i = i + 2) {
-  console.log(i);
+// for (let i = 1; i <= 100; i = i + 2) {
+//   console.log(i);
+// }
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+  console.log("sum =", sum, "i =", i, "sum + i =", sum + i);
+  sum = sum + i;
 }
+console.log("sum =", sum);

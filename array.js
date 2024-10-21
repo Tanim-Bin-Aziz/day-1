1960462fc2a74b7286a32a81a52b20c3f1957746
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-21 13:17:52 +0600

    dynamic array handle test done

--- array.js
+++ array.js
@@ -1,11 +1,13 @@
 const anda = [10, 20, 30, 40, 50, 60, 70, 80];
 const team = ["sakib", "shetab", "aksah", "ohi", "mehedi hasan"];
 // team[3] = "Asif";
-team[team.length] = "asif aman";
+team[team.length] = "asif aman"; /// dynamic array handle this is good one
 const arr = [];
 arr[0] = 1;
 arr[1] = 2;
 arr[2] = 3;
+arr[3] = 4;
+arr[4] = 5;
 arr[99] = 100;
 console.log(arr);
-console.log(team, anda.length);
+console.log(team, team[5], anda.length);

d6a4024fbf14b978d0766e1ed063bcecd4d692ee
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-21 13:02:10 +0600

    array testing

--- array.js
+++ array.js
@@ -1,3 +1,8 @@
 const anda = [10, 20, 30, 40, 50, 60, 70, 80];
 const team = ["sakib", "shetab", "aksah", "ohi"];
+const arr = [];
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+console.log(arr);
 console.log(team[3], anda.length);

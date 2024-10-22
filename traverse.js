3eaebdab75c759fb9a4dcf3b6782547fa01df7c9
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-22 20:35:47 +0600

    traverse element set

---
+++ traverse.js
@@ -0,0 +1,20 @@
+// Traverse Array Elements
+const arr = [1, 2, 3, 4, 5, 6];
+
+/**
+ * 1 2 3 4 5
+ * 0 1 2 3 4
+ *
+ * 'A' 'B' 'C' 'D'
+ *  0   1   2   3
+ *
+ * 1 B 3 5 F 0 true
+ * 0 1 2 3 4 5 6
+ *
+ */
+
+// simple Traverse
+
+// Array Sum & Avg
+
+// Find Large Number

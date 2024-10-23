63011b98977527ea1ce328d5718f837abae3dfb0
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-23 12:58:18 +0600

    array fill test

--- update.js
+++ update.js
@@ -11,6 +11,8 @@ for (let i = 0; i < arr1.length; i++) {
 // console.log(arr1);
 //Array Fill
 const arr2 = new Array(10);
+arr2.fill(0);
+console.log(arr2);
 
 const names = ["nayem", "shefa", "salvy"];
 

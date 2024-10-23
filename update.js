5df83bf06a276891567783fd16dd52a7078f969d
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-23 12:52:53 +0600

    Array element manual update

---
+++ update.js
@@ -0,0 +1,21 @@
+/**
+ * Update Array Elements and Fill Array
+ */
+
+//Array Fill Manual Way
+
+const arr1 = new Array(10);
+for (let i = 0; i < arr1.length; i++) {
+  arr1[i] = false;
+}
+// console.log(arr1);
+//Array Fill
+const arr2 = new Array(10);
+
+const names = ["nayem", "shefa", "salvy"];
+
+// console.log(names);
+
+// Fill Array and Update
+
+//Array is mutable

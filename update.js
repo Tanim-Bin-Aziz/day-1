9fdcbbf6110166ae43fc76ce020ecdb5fdd95325
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-23 13:04:12 +0600

    array manual update more test

--- update.js
+++ update.js
@@ -12,10 +12,15 @@ for (let i = 0; i < arr1.length; i++) {
 //Array Fill
 const arr2 = new Array(10);
 arr2.fill(0);
-console.log(arr2);
+// console.log(arr2);
 
 const names = ["nayem", "shefa", "salvy"];
 
+names[0] = "hm nayem";
+names[1] = "shefa taranjum";
+names[2] = "salvy parker";
+
+console.log(names);
 // console.log(names);
 
 // Fill Array and Update

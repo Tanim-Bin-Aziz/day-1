2b351d0dfa09b147306bf5e3df3a6049bcf3e994
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-27 13:36:36 +0600

    multi dimensonal way test

--- anything.js
+++ anything.js
@@ -3,7 +3,7 @@
  */
 
 const arr = [10, "Ten", true, getTen, { ten: 10 }, [10, 10]];
-console.log(arr);
+// console.log(arr);
 
 function getTen() {
   return 10;
@@ -30,15 +30,14 @@ const times = (a, b) => a * b;
 const div = (a, b) => a / b;
 const mod = (a, b) => a % b;
 
+// Loop and call all fucntions from array
 const funcs = [sum, sub, times, div, mod];
 const a = 10,
   b = 20;
 for (let i = 0; i < funcs.length; i++) {
   const result = funcs[i](a, b);
-  console.log(`[${funcs[i].name}] Result = ${result}`);
+  // console.log(`[${funcs[i].name}] Result = ${result}`);
 }
-
-// Loop and call all fucntions from array
 
 // Array of Arrays - multi Dimensonal Array
 
@@ -48,3 +47,17 @@ const pointTable = [
   [5, 7],
   [10, 23],
 ];
+
+// One Dimensonal Traverse Way
+for (let i = 0; i < pointTable.length; i++) {
+  // console.log(`Point ${i} - x=${pointTable[i][0]} and y=${pointTable[i][1]}`);
+}
+
+// Two Dimensonal Traverse Way
+
+const numbers = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+console.log(numbers[0]);

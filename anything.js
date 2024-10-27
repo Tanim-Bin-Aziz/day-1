2adc6adb49a9038cc432d5b65c7128e4e6ec2bee
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-27 13:42:53 +0600

    two dimensonal way test

--- anything.js
+++ anything.js
@@ -60,4 +60,13 @@ const numbers = [
   [4, 5, 6],
   [7, 8, 9],
 ];
-console.log(numbers[0]);
+for (let i = 0; i < numbers.length; i++) {
+  for (let j = 0; j < numbers.length; j++) {
+    console.log(numbers[i][j]);
+  }
+}
+// for (let j = 0; j < numbers.length; j++) {
+//   console.log(numbers[1][j]);
+// }
+
+// console.log(numbers[1][0]);

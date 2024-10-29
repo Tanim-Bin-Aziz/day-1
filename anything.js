478992c4273384b883a3d409411a1da2bb4bcdcb
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-29 18:25:46 +0600

    matrix test done

--- anything.js
+++ anything.js
@@ -76,3 +76,30 @@ for (let i = 0; i < pointTable.length; i++) {
     console.log(`Point [${i},${j}] = ${pointTable[i][j]}`);
   }
 }
+
+// Matrix example
+const matrixA = [
+  [1, 3],
+  [1, 0],
+  [1, 2],
+];
+const matrixB = [
+  [0, 0],
+  [7, 5],
+  [2, 1],
+];
+
+const matrixSum = (matrixA, matrixB) => {
+  const result = [];
+  for (let i = 0; i < matrixA.length; i++) {
+    const row = [];
+    for (let j = 0; j < matrixA[i].length; j++) {
+      row.push(matrixA[i][j] + matrixB[i][j]);
+    }
+    result.push(row);
+  }
+  return result;
+};
+
+const matrixC = matrixSum(matrixA, matrixB);
+console.log(matrixC);

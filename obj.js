2a1530adf6076b1d788275005a9cedb001596378
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-29 18:41:06 +0600

    object testing

---
+++ obj.js
@@ -0,0 +1,22 @@
+// oject is the key value pair to store data
+
+const fName = "hm";
+const lName = "nayem";
+const email1 = "tanimbinaziz@gmail.com";
+
+const fName2 = "shefa";
+const lName2 = "taranjum";
+const email2 = "shefa12@gmail.com";
+
+const person = ["hm", "shefa", "salvy"];
+
+// const o = {};
+// console.log(typeof o);
+
+const person1 = {};
+person1.firstName = "Tanim";
+person1.lastName = "Bin Aiz";
+person1.email = "tanimbinziz145@gmail.com";
+
+console.log(person1);
+console.log(person1.firstName);

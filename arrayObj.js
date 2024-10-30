74f95fd953347101ec6f747cd5554184407cac01
Tanim-Bin-Aziz <tanimbinaziz145@gmail.com>
2024-10-30 12:52:09 +0600

    array object access test

---
+++ arrayObj.js
@@ -0,0 +1,21 @@
+const p1 = {
+  name: "tanim bin aziz",
+  age: 197,
+  phone: "01719000060",
+  email: "tanimbinaziz1245@gmail.com",
+};
+const p2 = {
+  name: "tanim bin maow",
+  age: 197,
+  phone: "01719000989084060",
+  email: "tanimbinaziz1245@gmail.com",
+};
+const p3 = {
+  name: "tanim bin",
+  age: 197,
+  phone: "01719083277400060",
+  email: "tanimbinaziz1245@gmail.com",
+};
+
+const people = [p1, p2, p3];
+console.log(people[1]);
